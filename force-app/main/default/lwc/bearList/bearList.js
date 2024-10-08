6300b98a781c4404b7c39d9d4d8065535b942e5c
Emdad-Pc <emdad@ibos.io>
2024-10-08 12:00:00 +0600

    bear project complete

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,11 +1,24 @@
+import { publish, MessageContext } from 'lightning/messageService';
+import BEAR_LIST_UPDATE_MESSAGE from '@salesforce/messageChannel/BearListUpdate__c';
 import { LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 import searchBears from '@salesforce/apex/BearController.searchBears';
-export default class BearList extends LightningElement {
+export default class BearList extends NavigationMixin(LightningElement) {
 	searchTerm = '';
-	@wire(searchBears, {searchTerm: '$searchTerm'})
 	bears;
-	error;
+	@wire(MessageContext) messageContext;
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	loadBears(result) {
+	this.bears = result;
+	if (result.data) {
+		const message = {
+		bears: result.data
+		};
+		publish(this.messageContext, BEAR_LIST_UPDATE_MESSAGE, message);
+	}
+	}
+	
 	appResources = {
 		bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
 	};
